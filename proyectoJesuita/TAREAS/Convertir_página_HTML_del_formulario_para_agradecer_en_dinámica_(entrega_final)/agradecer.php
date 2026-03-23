f1d4d2d79f6c2e17f5dc192775ebe922ad577388
LauraMunozSanchez <lmunozsanchez.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 17:44:44 +0100

    ayudax2

--- proyectoJesuita/TAREAS/Convertir_página_HTML_del_formulario_para_agradecer_en_dinámica_(entrega_final)/agradecer.php
+++ proyectoJesuita/TAREAS/Convertir_página_HTML_del_formulario_para_agradecer_en_dinámica_(entrega_final)/agradecer.php
@@ -10,16 +10,14 @@
     }
     function mostraralumnos(){ 
 	    $conexion=conectar();  
-	    $sql = "SELECT nombre, IDAlumno FROM alumnos";
+	    $sql = "SELECT nombre, IDAlumno FROM alumnos;";
 	    $resultado=$conexion->query($sql);
-		$totalFilas = $resultado->num_rows; // Obtener el número total de filas en el resultado
 		$fila = $resultado->fetch_array(); // Obtener la primera fila del resultado
 		while ($fila) { // Iterar mientras haya filas disponibles
 			echo '<option value="' . $fila["IDAlumno"] . '">' . $fila["nombre"] . '</option>';
 			$fila = $resultado->fetch_array(); // Obtener la siguiente fila
         }
 		$conexion->close(); // Cerrar la conexión después de usarla
-		return $totalFilas; // Devolver el número total de filas pasadas por el resultado
     }
 ?>
 
@@ -54,6 +52,7 @@
                 <label>Este agradecimiento es para:</label>
                 <select name="nombres" id="nombres">
                     <option value="" disabled selected>--Selecciona a un compañero--</option>
+                    <option value="00" ></option>
                     <?php
                         mostraralumnos();
                     ?>
