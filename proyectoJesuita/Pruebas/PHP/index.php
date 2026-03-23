64cc22fa664d29934facb9df922bfb2db75dc6eb
LauraMunozSanchez <lmunozsanchez.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 17:47:01 +0100

    Delete proyectoJesuita directory
    Borro todo para reorganizarlo

--- proyectoJesuita/Pruebas/PHP/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require("funcionesAlumnos.php"); 
-
-mostrarAlumnos();
-?>
--- proyectoJesuita/Pruebas/SQL/crearBBDD.sql
+++
@@ -1,5 +0,0 @@
--- CREACION DE LA BASE DE DATOS agradeceEnCompania
-CREATE DATABASE agradeceEnCompania;
-
--- USO DE LA BASE DE DATOS agradeceEnCompania
-USE agradeceEnCompania;
