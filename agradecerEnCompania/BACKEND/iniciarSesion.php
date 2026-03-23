b7c265c0a815c7cc3c9d34c5a2ffe89f0ea1fafe
LauraMunozSanchez <lmunozsanchez.guadalupe@alumnado.fundacionloyola.net>
2026-03-24 00:50:50 +0100

    Update iniciarSesion.php

--- agradecerEnCompania/BACKEND/iniciarSesion.php
+++ agradecerEnCompania/BACKEND/iniciarSesion.php
@@ -3,7 +3,7 @@
     $usuario = $_POST['nombre'];
     $pw = $_POST['clave'];
 
-    $sql = "SELECT * FROM alumnos WHERE alumno='$usuario' AND password='$pw';";
+    $sql = "SELECT * FROM alumnos WHERE nombre='$usuario' AND clave='$pw';";
 
     //echo $sql; -> mostrar la consulta SQL para verificar que se ha construido correctamente y probarlo en la base de datos. Si la consulta es correcta, se puede eliminar esta línea.
 
