722d6118222a46c64c0f08e4168177ebbad5a54e
LauraMunozSanchez <lmunozsanchez.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 13:40:54 +0100

    coreccion

--- proyectoJesuita/Pruebas/PHP/agradecerP.php
+++ proyectoJesuita/Pruebas/PHP/agradecerP.php
@@ -1,26 +1,28 @@
 <!-- CREAR AGRADECIMIENTOS -->
 <?php
-    require("configdb.php");
+    define("SERVIDOR",'localhost');
+    define("USUARIO",'root');
+    define("PASSWORD",'');
+    define("BBDD",'agradeceencompania');
+
     function conectar(){
 	    $conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD);
 	    $conexion->set_charset("utf8"); 
         return $conexion;
     }
-     function mostraralumnos(){ 
+    function mostraralumnos(){ 
 	    $conexion=conectar();  
-	    // No es lo que pedia : $sql="SELECT * FROM alumnos"; // SELECT * FROM alumnos LIMIT 3 -> te devuelve las 3 primeras filas - CORREGIR CONSULTA TRAYENDO SOLO LAS DOS COLUMNAS
-		$sql = "SELECT nombre, IDAlumno FROM alumnos";
-		
-	    $totalFilas = $resultado->num_rows; // Obtener el número total de filas en el resultado
+	    $sql = "SELECT nombre, IDAlumno FROM alumnos";
+	    $resultado=$conexion->query($sql);
+		$totalFilas = $resultado->num_rows; // Obtener el número total de filas en el resultado
 		$fila = $resultado->fetch_array(); // Obtener la primera fila del resultado
 		while ($fila) { // Iterar mientras haya filas disponibles
-	        echo '<p>';
-	        echo 'Nombre alumno: '.$fila["nombre"];
-			echo 'Número de puesto: '.$fila["IDAlumno"];
-	        echo '</p>';
+			echo '<option value="' . $fila["IDAlumno"] . '">' . $fila["nombre"] . '</option>';
 			$fila = $resultado->fetch_array(); // Obtener la siguiente fila
-		}
-     }
+        }
+		$conexion->close(); // Cerrar la conexión después de usarla
+		return $totalFilas; // Devolver el número total de filas pasadas por el resultado
+    }
 ?>
 
 <!DOCTYPE html>
